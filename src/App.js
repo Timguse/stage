1a60c5e8596847a0bda231d5bdac5cd1383229ea
Timguse <88061648+Timguse@users.noreply.github.com>
2021-10-01 09:06:59 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Table from './table.js';
+import Form from './form.js';
+
+class App extends Component {
+    state = {
+        characters: [],
+    }
+    removeCharacter = (index) => {
+        const { characters } = this.state
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        })
+    }
+
+    render() {
+        const { characters } = this.state
+
+        return (
+            <div className="container">
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
+                <Form />
+            </div>
+        )
+    }
+    handleSubmit = (character) => {
+        this.setState({ characters: [...this.state.characters, character] })
+    }
+}
+
+export default App
